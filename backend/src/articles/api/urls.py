a6083c047e0625879e84133391ea6e69d513f2da
Jordan Engstrom <jordan.engstrom@gmail.com>
2020-01-22 20:50:00 -0700

    implemented all CRUD operations using Django's rest framework APIViews

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView
+from .views import (
+    ArticleListView,
+    ArticleDetailView,
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleDestroyView,
+)
 
 urlpatterns = [
     path('', ArticleListView.as_view()),
-    path('<pk>', ArticleDetailView.as_view()),
+    path('create/', ArticleCreateView.as_view()),
+    path('<pk>/', ArticleDetailView.as_view()),
+    path('<pk>/update/', ArticleUpdateView.as_view()),
+    path('<pk>/delete/', ArticleDestroyView.as_view()),
 ]
